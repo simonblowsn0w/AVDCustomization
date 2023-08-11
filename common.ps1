10a65cc600a8075089da9b82c8375a2c1b6beb64
SimonLeung2022 <heiviomagi@gmail.com>
2023-08-11 22:09:08 +0100

    Update common.ps1

--- common.ps1
+++ common.ps1
@@ -6,7 +6,7 @@ Write-Host "Chocolately installation completed....."
 Write-Host "Install FireFox..."
 Start-Process `
     -FilePath "C:\ProgramData\chocolatey\bin\choco.exe" `
-    -ArgumentList "install firefoxesr --version 78.15.0 --params=`"/NoAutoUpdate /NoMaintenanceService`" -requirechecksum --no-progress -y" `
+    -ArgumentList "install firefoxesr --version 78.15.0 --params `"/NoAutoUpdate /NoMaintenanceService`" -requirechecksum --no-progress -y" `
     -Wait `
     -Passthru
 
