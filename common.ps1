018ff870551b1749f0f1eeb09eece379105508a7
SimonLeung2022 <102918258+SimonLeung2022@users.noreply.github.com>
2023-08-11 18:12:45 +0100

    Update common.ps1

--- common.ps1
+++ common.ps1
@@ -1,11 +1,16 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('http://internal/odata/repo/ChocolateyInstall.ps1'))
+Write-Host "Install Chocolately..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-WebRequest -Uri https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Write-Host "Chocolately installation completed....."
 
+Write-Host "Install FireFox..."
 Start-Process `
     -FilePath "C:\ProgramData\chocolatey\bin\choco.exe" `
     -ArgumentList "firefoxesr --version 78.15.0 --params='\"/NoAutoUpdate /NoMaintenanceService\"' -requirechecksum --no-progress -my" `
     -Wait `
     -Passthru
 
+Write-Host "Install Notepadplusplus..."
     Start-Process `
     -FilePath "C:\ProgramData\chocolatey\bin\choco.exe" `
     -ArgumentList "install notepadplusplus -requirechecksum --no-progress -my" `
